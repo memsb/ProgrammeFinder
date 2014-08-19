6a8a766749bb0142f969f0db2c5bae5e728f38cb
martin <mbuckley@gmail.com>
2014-08-19 04:29:45 +0100

    e2e and unit tested

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,35 @@
-$(document).ready(function () {
-    $(".episode-search-field").change(function () {
-        search($(this).val());
-    });
-    $(".episode-search-button").click(function () {
-        search($(this).prev('input').val());
-    });
-});
-
-function search(query) {
-    $.ajax({
-        // pass in to script
-        url: "http://localhost/Workspace/ProgrammeFinder/public/search/limit/10/query/" + query,
-        context: document.body
-    }).done(function (data) {
-            //change to populate nearest results
-            if (data) {
-                $(".episode-search-results").html(data);
+// script params injected
+define(['params'], function (params) {
+    $(document).ready(function () {
+        var timeout;
+        $("#searchBox").keyup( function() {
+            clearTimeout(timeout);
+            var that = this;
+            var search_string = $(this).val();
+            if (search_string !== '') {
+                timeout = setTimeout(function(){
+                    search(that, search_string);
+                }, 1000);
             }
         });
-}
+        $(".episode-search-button").click(function () {
+            $(this).preventDefault();
+            search(this, $(this).prev('input').val());
+        });
+    });
+
+    // search request populates only one widget
+    function search(source, query) {
+        if (query) {
+            $.ajax({
+                url: params.searchUrl + '/' + query,
+                context: document.body
+            }).done(function (data) {
+                    //change to populate nearest results
+                    if (data) {
+                        $(source).closest(".episode-search").find(".episode-search-results ul").html(data);
+                    }
+                });
+        }
+    }
+});
